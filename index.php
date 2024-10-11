756e1663afa0b4fec71488b4a4522b0b88a9d4a5
angelaviluron <angelaagustina04@gmail.com>
2024-10-11 16:51:42 -0300

    estilo

--- index.php
+++ index.php
@@ -3,9 +3,40 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>VeryDeli</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="estilo.css">
 </head>
 <body>
     
+<div class="container" id="contenido">
+
+<nav class="navbar bg-body-tertiary">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">
+
+
+      <img src="https://cdn.pixabay.com/photo/2016/04/22/17/36/wooden-v-1346205_1280.png" alt="Logo" width="30" height="24" class="d-inline-block align-text-top">
+      VeryDeli
+
+      <form class="d-flex" role="search">
+      <input class="form-control me-2" type="search" placeholder="Buscar publicacion" aria-label="Buscar publicacion">
+      <button class="btn btn-outline-success" type="submit">Buscar</button>
+    </form>
+    </a>
+  </div>
+</nav>
+
+
+
+
+
+
+
+</div>
+
+
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
 </html>
