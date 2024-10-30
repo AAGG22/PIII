73c040705386373e6f5be1144a4ebc21fb035178
Gabi <40302142+kuhg@users.noreply.github.com>
2024-10-30 20:09:26 -0300

    Add files via upload
    Aca dejo el formulario de publicacion con la logica que carga la misma a la base de datos y el sql con las modificaciones que le hice a la tabla de publicacion

--- crearPublicacion.php
+++ crearPublicacion.php
@@ -1,78 +1,95 @@
 <?php
-  session_start();
-?>
-<!doctype html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Crear publicacion</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-  </head>
-  <body>
-  
-<?php
-include('cabecera.php');
-?>
+    include 'coneccion/conexionPDO.php';
+    //Control de que los datos estan ingresados
+    try {
 
-        <div class="card container" style='width:750px'>
-        <div class="card-header" align="center" style='background-color: lightblue;'>
-            <strong>Crear Publicacion</strong>
-        </div>
-        <div class="card-body">
-        <div class="container ">
+        if(isset($_POST['titulo']) && isset($_POST['descripcion']) && isset($_POST['peso']) && isset($_POST['volumen']) && isset($_POST['contactoDestino'])
+        && isset($_POST['provinciaOrigen']) && isset($_POST['ciudadOrigen']) && isset($_POST['direccionOrigen'])
+        && isset($_POST['provinciaDestino']) && isset($_POST['ciudadDestino']) && isset($_POST['direccionDestino'])) {
+
+            $titulo = $_POST['titulo'];
+            $descripcion = $_POST['descripcion'];
+            $peso = $_POST['peso'];
+            $volumen = $_POST['volumen'];
+            $contacto = $_POST['contactoDestino'];
+            $provinciaOrigen = $_POST['provinciaOrigen'];
+            $ciudadOrigen = $_POST['ciudadOrigen'];
+            $direccionOrigen = $_POST['direccionOrigen'];
+            $provinciaDestino = $_POST['provinciaDestino'];
+            $ciudadDestino = $_POST['ciudadDestino'];
+            $direccionDestino = $_POST['direccionDestino'];
+            $fechaActual = new DateTime();
+            $fechaFormateada = $fechaActual->format('Y-m-d H:i:s');
+            $imagen = '';
+            $idUsuario = 14;
+
+            if(isset($_FILES['imagenPaquete'])){
+                //tomo datos del archivo para poder hacer controles despues
+                $foto = $_FILES['imagenPaquete'];
+                $nombre = $foto['name'];
+                $tipo = $foto['type'];
+                $ruta = $foto['tmp_name'];
+                $size = $foto['size'];
+                $dimensiones = getimagesize($ruta);
+                $width = $dimensiones[0];
+                $height = $dimensiones[1];
+                $carpeta = "foto/";
             
-        <form action="subirPublicacion.php" method="post" enctype="multipart/form-data">
-            <div class="form-row">
-            <div class="form-group col-md-6">
-                    <label for="titulo">Titulo:</label>
-                    <input type="text" class="form-control" id="titulo" name="titulo">
-                </div>
-                <div class="form-group col-md-6">
-                    <label for="origen">Origen:</label>
-                    <input type="text" class="form-control" id="origen" name="origen">
-                </div>
-                <div class="form-group col-md-6">
-                    <label for="destino">Destino:</label>
-                    <input type="text" class="form-control" id="destino" name="destino">
-                </div>
                 
-                <div class="form-group col-md-6">
-                    <label for="descripcion">Descripcion:</label>
-                    <input type="text" class="form-control" id="descripcion" name="descripcion">
-                </div>
-                
-            </div>
-            
-            <div class="form-row">
-                <div class="form-group col-md-6">
-                    <label for="volumen">Volumen: </label>
-                    <input type="text" class="form-control" id="volumen" name="volumen">
-                </div>
-                <div class="form-group col-md-6">
-                    <label for="peso">Peso:</label>
-                    <input type="number" class="form-control" id="peso" name="peso">
-                </div><br>
-                <div class="form-group col-md-6">
-                    <label for="imagen">Imagen:   </label>
-                    <input type="file" name="imagen" id="imagen">
-                </div>
-            </div>
-            
-            <br><br>
-            <div align="right">
-                <button type="submit" class="btn btn-primary">Crear</button>
-                <button type="button" class="btn btn-danger">Cancelar</button>
-            </div>
-        </form>
-        </div>
-        </div>
-    </div>
-
-<?php
-include('pie.php');
+                $allowed_types = ['image/jpg', 'image/jpeg', 'image/png', 'image/gif'];//controlo que sea una imagen y no otro tipo
+                if (!in_array($tipo, $allowed_types)) {
+                    echo "error, el archivo no es imagen";
+                    exit;
+                } else if ($size > 3 * 1024 * 1024) {//controlo que no supere el tamaño
+                    echo "error: el tamaño maximo permitido es de 3MB";
+                    exit;
+                } else {
+                    $src = $carpeta . $nombre;
+                    move_uploaded_file($ruta, $src);
+                    $imagen = "foto/" . $nombre;
+                }
+            }
+        
+    
+            $sql = "INSERT INTO publicacion
+            (pu_fk_u_id, pu_titulo, pu_fk_origen_provincia, pu_fk_origen_ciudad, pu_fk_origen_direccion,
+            pu_fk_destino_provincia, pu_fk_destino_ciudad, pu_fk_destino_direccion, pu_volumen, pu_peso,
+            pu_descripcion, pu_fecha, pu_imagen)
+            VALUES (:pu_fk_u_id, :pu_titulo, :pu_fk_origen_provincia, :pu_fk_origen_ciudad, :pu_fk_origen_direccion,
+            :pu_fk_destino_provincia, :pu_fk_destino_ciudad, :pu_fk_destino_direccion, :pu_volumen, :pu_peso,
+            :pu_descripcion, :pu_fecha, :pu_imagen)";
+    
+            $stmt = $pdo->prepare($sql);
+    
+            $stmt->bindParam(':pu_fk_u_id', $idUsuario);
+            $stmt->bindParam(':pu_titulo', $titulo);
+            $stmt->bindParam(':pu_descripcion', $descripcion);
+            $stmt->bindParam(':pu_peso', $peso);
+            $stmt->bindParam(':pu_volumen', $volumen);
+            $stmt->bindParam(':pu_fk_origen_provincia', $provinciaOrigen);
+            $stmt->bindParam(':pu_fk_origen_ciudad', $ciudadOrigen);
+            $stmt->bindParam(':pu_fk_origen_direccion', $direccionOrigen);
+            $stmt->bindParam(':pu_fk_destino_provincia', $provinciaDestino);
+            $stmt->bindParam(':pu_fk_destino_ciudad', $ciudadDestino);
+            $stmt->bindParam(':pu_fk_destino_direccion', $direccionDestino);
+            $stmt->bindParam(':pu_fecha', $fechaFormateada);
+            $stmt->bindParam(':pu_imagen', $imagen);
+    
+            if ($stmt->execute()) {
+                echo "Archivo y datos guardados en la base de datos exitosamente.";
+                include 'coneccion/cerrarConexionPDO.php';
+                header('location: buscador.php');
+                die();
+            } else {
+                echo "Hubo un problema al subir el archivo y los datos.";
+                include 'coneccion/cerrarConexionPDO.php';
+                header('location: buscador.php');
+                die();
+            }
+        } else {
+            echo "Faltan archivos para la publicacion";
+        }
+    } catch (PDOException $e) {
+        echo "Error: " . $e->getMessage();
+    }
 ?>
-
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
-  </body>
-</html>
