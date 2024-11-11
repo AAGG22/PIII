7b054b6fe717237c3268f89985b1ac8ea765889e
angelaviluron <angelaagustina04@gmail.com>
2024-11-11 07:58:04 -0300

    subir publicacion BD

--- crearPublicacion.php
+++ crearPublicacion.php
@@ -1,95 +1,120 @@
 <?php
-    include 'coneccion/conexionPDO.php';
-    //Control de que los datos estan ingresados
-    try {
-
-        if(isset($_POST['titulo']) && isset($_POST['descripcion']) && isset($_POST['peso']) && isset($_POST['volumen']) && isset($_POST['contactoDestino'])
-        && isset($_POST['provinciaOrigen']) && isset($_POST['ciudadOrigen']) && isset($_POST['direccionOrigen'])
-        && isset($_POST['provinciaDestino']) && isset($_POST['ciudadDestino']) && isset($_POST['direccionDestino'])) {
-
-            $titulo = $_POST['titulo'];
-            $descripcion = $_POST['descripcion'];
-            $peso = $_POST['peso'];
-            $volumen = $_POST['volumen'];
-            $contacto = $_POST['contactoDestino'];
-            $provinciaOrigen = $_POST['provinciaOrigen'];
-            $ciudadOrigen = $_POST['ciudadOrigen'];
-            $direccionOrigen = $_POST['direccionOrigen'];
-            $provinciaDestino = $_POST['provinciaDestino'];
-            $ciudadDestino = $_POST['ciudadDestino'];
-            $direccionDestino = $_POST['direccionDestino'];
-            $fechaActual = new DateTime();
-            $fechaFormateada = $fechaActual->format('Y-m-d H:i:s');
-            $imagen = '';
-            $idUsuario = 14;
-
-            if(isset($_FILES['imagenPaquete'])){
-                //tomo datos del archivo para poder hacer controles despues
-                $foto = $_FILES['imagenPaquete'];
-                $nombre = $foto['name'];
-                $tipo = $foto['type'];
-                $ruta = $foto['tmp_name'];
-                $size = $foto['size'];
-                $dimensiones = getimagesize($ruta);
-                $width = $dimensiones[0];
-                $height = $dimensiones[1];
-                $carpeta = "foto/";
-            
-                
-                $allowed_types = ['image/jpg', 'image/jpeg', 'image/png', 'image/gif'];//controlo que sea una imagen y no otro tipo
-                if (!in_array($tipo, $allowed_types)) {
-                    echo "error, el archivo no es imagen";
-                    exit;
-                } else if ($size > 3 * 1024 * 1024) {//controlo que no supere el tamaño
-                    echo "error: el tamaño maximo permitido es de 3MB";
-                    exit;
-                } else {
-                    $src = $carpeta . $nombre;
-                    move_uploaded_file($ruta, $src);
-                    $imagen = "foto/" . $nombre;
-                }
-            }
-        
-    
-            $sql = "INSERT INTO publicacion
-            (pu_fk_u_id, pu_titulo, pu_fk_origen_provincia, pu_fk_origen_ciudad, pu_fk_origen_direccion,
-            pu_fk_destino_provincia, pu_fk_destino_ciudad, pu_fk_destino_direccion, pu_volumen, pu_peso,
-            pu_descripcion, pu_fecha, pu_imagen)
-            VALUES (:pu_fk_u_id, :pu_titulo, :pu_fk_origen_provincia, :pu_fk_origen_ciudad, :pu_fk_origen_direccion,
-            :pu_fk_destino_provincia, :pu_fk_destino_ciudad, :pu_fk_destino_direccion, :pu_volumen, :pu_peso,
-            :pu_descripcion, :pu_fecha, :pu_imagen)";
-    
-            $stmt = $pdo->prepare($sql);
-    
-            $stmt->bindParam(':pu_fk_u_id', $idUsuario);
-            $stmt->bindParam(':pu_titulo', $titulo);
-            $stmt->bindParam(':pu_descripcion', $descripcion);
-            $stmt->bindParam(':pu_peso', $peso);
-            $stmt->bindParam(':pu_volumen', $volumen);
-            $stmt->bindParam(':pu_fk_origen_provincia', $provinciaOrigen);
-            $stmt->bindParam(':pu_fk_origen_ciudad', $ciudadOrigen);
-            $stmt->bindParam(':pu_fk_origen_direccion', $direccionOrigen);
-            $stmt->bindParam(':pu_fk_destino_provincia', $provinciaDestino);
-            $stmt->bindParam(':pu_fk_destino_ciudad', $ciudadDestino);
-            $stmt->bindParam(':pu_fk_destino_direccion', $direccionDestino);
-            $stmt->bindParam(':pu_fecha', $fechaFormateada);
-            $stmt->bindParam(':pu_imagen', $imagen);
-    
-            if ($stmt->execute()) {
-                echo "Archivo y datos guardados en la base de datos exitosamente.";
-                include 'coneccion/cerrarConexionPDO.php';
-                header('location: buscador.php');
-                die();
-            } else {
-                echo "Hubo un problema al subir el archivo y los datos.";
-                include 'coneccion/cerrarConexionPDO.php';
-                header('location: buscador.php');
-                die();
-            }
-        } else {
-            echo "Faltan archivos para la publicacion";
-        }
-    } catch (PDOException $e) {
-        echo "Error: " . $e->getMessage();
+    include('cabecera.php');
+    if(!isset($_SESSION['user_id'])){
+        header('Location: loguin_formulario.php');
     }
+?>
+<h2 align="center">Crear Publicacion</h2>
+  
+<div class="container form-container">
+    
+    
+    <form class="row g-3" action="subirPublicacion.php" method="post" enctype="multipart/form-data">
+
+        <div class="col-10">
+            <label for="titulo" class="form-label">Titulo</label>
+            <input type="text" class="form-control" id="titulo" name="titulo" required>
+        </div>
+
+        <div class="col-10">
+            <label for="descripcion" class="form-label">Descripcion</label>
+            <textarea class="form-control" id="descripcion" name="descripcion" rows="3"></textarea >
+        </div>
+
+        <div class="col-md-5">
+            <label for="peso" class="form-label">Peso en kg</label>
+            <input type="number" class="form-control" id="peso" name="peso" required>
+        </div>
+
+        <div class="col-md-5">
+            <label for="volumen" class="form-label">Volumen (cm³)</label>
+            <input type="number" class="form-control" id="volumen" name="volumen" required>
+        </div>
+        <br>
+        <div class="col-10">
+            <label for="imagenPaquete" class="form-label">Subir una imagen</label>
+            <input type="file" class="form-control" id="imagenPaquete" name="imagenPaquete" >
+            
+        </div>
+
+
+        <div class="col-10">
+            <label for="nombreContactoDestino" class="form-label">Nombre destinatario</label>
+            <input type="text" class="form-control" id="nombreContactoDestino" name="nombreContactoDestino" >
+        </div>
+
+        <div class="col-10">
+            <label for="contactoDestino" class="form-label">Contacto destino</label>
+            <input type="number" class="form-control" id="contactoDestino" name="contactoDestino" >
+        </div>
+        
+        <!-- Origen -->
+        <h2>Origen</h2>
+        <div class="col-md-5">
+            <label for="provinciaOrigen" class="form-label">Provincia</label>
+            <select id="provinciaOrigen" name="provinciaOrigen" class="form-select" required>
+                <?php
+               
+                $sql = "SELECT * FROM argentina";
+                $stmt = $pdo->query($sql);
+                $resultado = $stmt->fetchAll(PDO::FETCH_NUM);
+
+                if ($resultado) {
+                foreach ($resultado as $row) {
+                    echo "<option value='".$row[0]."'>".$row[1]."</option>";
+                }
+                }
+            ?>
+            </select>
+        </div>
+
+        <div class="col-md-5">
+            <label for="ciudadOrigen" class="form-label">Ciudad</label>
+            <input type="text" class="form-control" id="ciudadOrigen" name="ciudadOrigen" required>
+        </div>
+
+        <div class="col-10">
+            <label for="direccionOrigen" class="form-label">Direccion</label>
+            <input type="text" class="form-control" id="direccionOrigen" name="direccionOrigen" required>
+        </div>
+
+        <!-- Destino -->
+        <br>
+        <h2>Destino</h2>
+        <br>
+        <div class="col-md-5">
+            <label for="provinciaDestino" class="form-label">Provincia</label>
+            <select id="provinciaDestino" name="provinciaDestino" class="form-select" required>
+                <?php
+                        $sql = "SELECT * FROM argentina";
+                        $stmt = $pdo->query($sql);
+                        $resultado = $stmt->fetchAll(PDO::FETCH_NUM);
+
+                        if ($resultado) {
+                        foreach ($resultado as $row) {
+                            echo "<option value='".$row[0]."'>".$row[1]."</option>";
+                        }
+                        }
+                        
+                ?>
+            </select>
+        </div>
+
+        <div class="col-md-5">
+            <label for="ciudadDestino" class="form-label">Ciudad</label>
+            <input type="text" class="form-control" id="ciudadDestino" name="ciudadDestino"  required>
+        </div>
+
+        <div class="col-10">
+            <label for="direccionDestino" class="form-label">Direccion</label>
+            <input type="text" class="form-control" id="direccionDestino" name="direccionDestino" required>
+        </div>
+        <br>
+        <div class="col-10">
+            <button type="submit" class="btn btn-primary" id="btnPublicar" name="btnPublicar">Publicar</button>
+        </div>
+    </form>
+</div>
+<?php
+    include 'pie.php'
 ?>
